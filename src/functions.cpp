0ec875d683306afad546e876bb4625f772ffec0b
Mauri <mauriciowest@gmail.com>
2021-06-08 16:20:09 -0300

    Esta funcionando y cambia de color dentro del string  Please enter the commit message for your changes. Lines starting

--- src/functions.cpp
+++ src/functions.cpp
@@ -2,8 +2,13 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
+#include <vector>
+
+#include "ctc.h"
 using namespace std;
 
+//using namespace std::stoi;
+//using namespace std::stoi;
 
 namespace{
 	const char *spaces = " \t";
@@ -36,6 +41,46 @@ bool isNumber(const char *cstr){
 	return n > 0;
 }
 
+void changeConlorWithString(string colorStr) {
+	int col1 ,col2;
+	string sCol1, sCol2;
+
+	int color2 = 0;
+	int newPos = 0;
+	//char* resArray,strChar;
+	//strChar = colorStr.c_str();
+//	strChar = &colorStr[0];
+
+//	resArray = strtok(strChar, ",");
+	//atoi(hours0.c_str());
+
+	
+	if ((newPos = colorStr.find(","))!=-1) {
+		sCol1 = colorStr.substr(0, newPos);
+		sCol2 = colorStr.substr(newPos + 1);
+
+		col1= atoi(sCol1.c_str());
+		col2 = atoi(sCol2.c_str());
+		//color1 = stoi(colorStr.substr(0, newPos));
+		//color2 = stoi(colorStr.substr(newPos+1));
+
+		SetColor(col1, col2);
+	}
+	/*
+		
+		char strChar[colorStr.length() + 1];
+
+	strcpy(strChar, colorStr.c_str());
+
+	while (resArray != NULL){
+		printf("%s\n", resArray);
+		pch = strtok(NULL, " ,.-");
+	}
+		*/
+	//SetColor(color1, color2);
+}
+
+
 
 void anlizeString(){
 	int count_find=0;
@@ -44,46 +89,32 @@ void anlizeString(){
 
 	string str,result1,result2,result3,result4;
 	string subStr,trimStr;
-	str = "test de cadena ((4,0)) test de cadena ((4,0)) test de cadena ";
-	int oldPos = 0;
+	str = "((6,0))test de cadena ((4,0)) test de cadena ((2,0)) test de cadena ";
+	
 	int newPos = 0;
 
 	subStr = str;
 	int band = 1;
 	cout << str << endl;
 	while (band) {
-		if ((count_find == 0) and ((newPos = str.find("((")) != -1)) {
-			cout << endl << "-------------pase 1-------------" << endl << endl;
-
-			subStr = str.substr(0, newPos);
-			//subStr = str.substr(oldPos, newPos);
-			cout << "-" << subStr << "-" << endl;
-
-			str = str.substr(newPos + 2);
-			cout << "- str:" << str << "-" << endl;
-			oldPos = newPos;
-
-			count_find=1;
-			
-		}else if ((count_find == 1) and ((newPos = str.find("))")) != -1)) {
-			cout << endl << "-------------pase 2-------------" << endl << endl;
+		if ((count_find == 0) and ((newPos = str.find("((")) != -1)) {			
 			subStr = str.substr(0, newPos);			
-			cout << "-" << subStr << "-"<< endl;
-			str = str.substr(newPos+2);			
-			cout << "- str:" << str << "-" << endl;
-			//oldPos = newPos;
-			oldPos += 2;
+			cout << subStr << endl;
+			str = str.substr(newPos + 2);						
+			count_find=1;			
+		}else if ((count_find == 1) and ((newPos = str.find("))")) != -1)) {			
+			subStr = str.substr(0, newPos);			
+			cout << subStr << endl;
+			changeConlorWithString(subStr);
+			str = str.substr(newPos+2);												
 			count_find=0;
-
-		}else{
-			
-			cout << endl << "-------------pase 3-------------" << endl << endl;
-			cout << "-" << str << "-" << endl;
-			//cout << str << endl;
+		}else{					
+			cout << str << endl;			
 			band = 0;
 		}
 
 	}
+
 	/*
 	if ((count_find == 2) and ((newPos = str.find("))")) != -1)) {
 		cout << "pase 2" << endl;
